932a48f7f05af517921806a986a75d3b58d3d96a
Martynexs <73691457+Martynexs@users.noreply.github.com>
2022-10-17 19:58:25 +0300

    Fix tests

--- src/test/java/ShoppingTest.java
+++ src/test/java/ShoppingTest.java
@@ -1,8 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,57 +15,56 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static junit.framework.TestCase.assertFalse;
 
-public class ShoppingTests {
-    private static final String m_loginFileName = "loginInfo.txt";
+public class ShoppingTest {
     private WebDriver m_driver;
-    private WebDriverWait m_wait;
+    private static String m_email;
+    private static String m_password;
 
-    @BeforeAll
-    public static void init() throws IOException {
+    @BeforeClass
+    public static void init()
+    {
         WebDriverManager.chromedriver().setup();
         registerUser();
     }
 
-    @BeforeEach
+    @Before
     public void setUp()
     {
-        WebDriverManager.chromedriver().setup();
-        m_driver = new ChromeDriver();
-        m_driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        m_wait = new WebDriverWait(m_driver, Duration.ofSeconds(10));
+        m_driver = getChromeDriver();
     }
 
-    @AfterEach
+    @After
     public void tearDown()
     {
         m_driver.quit();
     }
 
     @Test
-    public void test1() throws FileNotFoundException {
+    public void test1() throws FileNotFoundException
+    {
         var items = getItemsFromFile("data1.txt");
-        m_driver.get("https://demowebshop.tricentis.com/");
         logIn(m_driver);
         m_driver.findElement(By.xpath("//li[@class = 'inactive']/a[@href = '/digital-downloads']")).click();
-        orderItemsFromList(m_driver, m_wait, items);
+        orderItemsFromList(m_driver, items);
         assertFalse(m_driver.findElements(By.xpath("//strong[contains(text(), 'Your order has been successfully processed!')]")).isEmpty());
     }
 
     @Test
-    public void test2() throws FileNotFoundException {
+    public void test2() throws FileNotFoundException
+    {
         var items = getItemsFromFile("data2.txt");
-        m_driver.get("https://demowebshop.tricentis.com/");
         logIn(m_driver);
         m_driver.findElement(By.xpath("//li[@class = 'inactive']/a[@href = '/digital-downloads']")).click();
-        orderItemsFromList(m_driver, m_wait, items);
-        assertTrue(m_driver.findElements(By.xpath("//strong[contains(text(), 'Your order has been successfully processed!')]")).isEmpty());
+        orderItemsFromList(m_driver, items);
+        assertFalse(m_driver.findElements(By.xpath("//strong[contains(text(), 'Your order has been successfully processed!')]")).isEmpty());
     }
 
-    private void orderItemsFromList(WebDriver driver, WebDriverWait wait, List<String> items)
+    private void orderItemsFromList(WebDriver driver, List<String> items)
     {
+        var wait = new WebDriverWait(m_driver, Duration.ofSeconds(10));
+
         var loadingBar = driver.findElement(By.className("ajax-loading-block-window"));
         for (var item : items) {
             driver.findElement(By.xpath(String.format("//div[@class = 'product-item']//h2[@class = 'product-title']/a[text() = '%s']/following::input[@value='Add to cart']", item))).click();
@@ -84,60 +84,51 @@ public class ShoppingTests {
             driver.findElement(By.id("BillingNewAddress_ZipPostalCode")).sendKeys(UUID.randomUUID().toString());
             driver.findElement(By.id("BillingNewAddress_PhoneNumber")).sendKeys(UUID.randomUUID().toString());
         }
-        else
-        {
-            driver.findElement(By.className("new-address-next-step-button")).click();
-        }
 
-        driver.findElement(By.className("new-address-next-step-button")).click();
-
-        var button = driver.findElement(By.className("payment-method-next-step-button"));
-        wait.until(ExpectedConditions.elementToBeClickable(button));
-        button.click();
-        driver.findElement(By.className("payment-info-next-step-button")).click();
-        driver.findElement(By.className("confirm-order-next-step-button")).click();
+        clickButton(driver, wait, "new-address-next-step-button");
+        clickButton(driver, wait, "payment-method-next-step-button");
+        clickButton(driver, wait, "payment-info-next-step-button");
+        clickButton(driver, wait, "confirm-order-next-step-button");
     }
 
-    private static void registerUser() throws IOException {
-        var email = UUID.randomUUID().toString() + "@email.net";
-        var password = UUID.randomUUID().toString();
+    private void clickButton(WebDriver driver, WebDriverWait wait, String className)
+    {
+        var button = driver.findElement(By.className(className));
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+        button.click();
+    }
 
-        var file = new File(m_loginFileName);
-        file.createNewFile();
-        var writer = new FileWriter(file);
-        writer.write(email + '\n');
-        writer.write(password);
-        writer.close();
+    private static void registerUser()
+    {
+        m_email = UUID.randomUUID() + "@email.net";
+        m_password = UUID.randomUUID().toString();
 
-        var driver = new ChromeDriver();
+        var driver = getChromeDriver();
         driver.get("https://demowebshop.tricentis.com/ ");
         driver.findElement(By.className("ico-login")).click();
         driver.findElement(By.className(("register-button"))).click();
         driver.findElement(By.xpath("//input[@id = 'gender-male']")).click();
         driver.findElement(By.xpath("//input[@id = 'FirstName']")).sendKeys(UUID.randomUUID().toString());
         driver.findElement(By.xpath("//input[@id = 'LastName']")).sendKeys(UUID.randomUUID().toString());
-        driver.findElement(By.xpath("//input[@id = 'Email']")).sendKeys(email);
-        driver.findElement(By.xpath("//input[@id = 'Password']")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@id = 'ConfirmPassword']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@id = 'Email']")).sendKeys(m_email);
+        driver.findElement(By.xpath("//input[@id = 'Password']")).sendKeys(m_password);
+        driver.findElement(By.xpath("//input[@id = 'ConfirmPassword']")).sendKeys(m_password);
         driver.findElement(By.className(("register-next-step-button"))).click();
         driver.findElement(By.className(("register-continue-button"))).click();
         driver.quit();
     }
 
-    private void logIn(WebDriver driver) throws FileNotFoundException {
-        var file = new File(m_loginFileName);
-        var reader = new Scanner(file);
-        var email = reader.nextLine();
-        var password = reader.nextLine();
-        reader.close();
-
+    private void logIn(WebDriver driver)
+    {
+        driver.get("https://demowebshop.tricentis.com/");
         driver.findElement(By.className("ico-login")).click();
-        driver.findElement(By.className("email")).sendKeys(email);
-        driver.findElement(By.className("password")).sendKeys(password);
+        driver.findElement(By.className("email")).sendKeys(m_email);
+        driver.findElement(By.className("password")).sendKeys(m_password);
         driver.findElement(By.className("login-button")).click();
     }
 
-    private List<String> getItemsFromFile(String fileName) throws FileNotFoundException {
+    private List<String> getItemsFromFile(String fileName) throws FileNotFoundException
+    {
         var items = new ArrayList<String>();
         var file = new File(fileName);
         var reader = new Scanner(file);
@@ -148,5 +139,14 @@ public class ShoppingTests {
         }
         reader.close();
         return items;
+    }
+
+    private static WebDriver getChromeDriver()
+    {
+        var options = new ChromeOptions();
+        options.setHeadless(true);
+        var driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        return driver;
     }
 }
